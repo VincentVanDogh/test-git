36902f5facf11d5614cad790effa5deffbebe1a1
Tobias Gacko <e11730105@tuwien.ac.at>
2023-01-18 19:24:45 +0100

    #4

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,7 @@
 public class MyClass {
     public static void main(String[] args) {
         String myStr = "Hello, world";
-        int = 23
+        int = 23;
+        Long = 21321321321;
     }
 }

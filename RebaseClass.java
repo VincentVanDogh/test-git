e20211437a8b5e7fef29577e66ea8382de6f21ea
Tobias Gacko <e11730105@tuwien.ac.at>
2023-11-07 11:44:56 +0100

    Additional class

---
+++ RebaseClass.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public class RebaseClass {
+    private String name;
+    private Date dob;
+    private String email;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 class Main {
     public static void main(String[] args) {
         String hi = "Hi";
+        String name = "Alfa";
+        int number = 10;
+        long longNumber = 20l;
     }
 }
